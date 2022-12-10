23e6782d13d49e033356fb4f6c7cf290773ddd86
LynxCatTheThird <tsfxlyx@163.com>
2022-12-10 19:05:11 +0800

    更新主题&&启用搜索与密码

--- README.md
+++ README.md
@@ -4,7 +4,5 @@
 
 1. 半自动部署脚本需要修改一下路径和换行符
 
-2. .gitignore 里面的“自用”“bak”等仅仅是因为 git 会强转换行符、为了方便自己使用而已，毫无私意，并非“特供版”
-
-3. 安装 hexo-html-minifier hexo-clean-css hexo-uglify hexo-renderer-pandoc hexo-filter-mathjax 并卸载 hexo-renderer-marked，还要安装 [Pandoc](https://pandoc.org/installing.html)
+2. 安装 *hexo-html-minifier hexo-clean-css hexo-uglify hexo-renderer-pandoc hexo-filter-mathjax  hexo-henerator-search-lite hexo-helper-crypto*  并卸载 *hexo-renderer-marked* ，还要安装 [Pandoc](https://pandoc.org/installing.html)
 
---
+++ person/themes/particlex/source/js/crypto.js
@@ -0,0 +1,42 @@
+const crypto = {
+    init(enc, sha) {
+        this.enc = enc;
+        this.sha = sha;
+        this.composition = false;
+        window.addEventListener("load", () => {
+            this.input = document.getElementsByClassName("crypto")[0];
+            this.content = document.getElementsByClassName("content")[0];
+            this.content.style.opacity = 0;
+            this.input.addEventListener("input", () => this.composition || this.update());
+            this.input.addEventListener("compositionstart", () => (this.composition = true));
+            this.input.addEventListener("compositionend", () => {
+                this.update(), (this.composition = false);
+            });
+        });
+    },
+    SHA(str) {
+        return CryptoJS.SHA256(str).toString(CryptoJS.enc.Base64);
+    },
+    decrypt(enc, key, sha) {
+        try {
+            let res = CryptoJS.AES.decrypt(enc, key, {
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7,
+            }).toString(CryptoJS.enc.Utf8);
+            return { dec: res, check: this.SHA(res) == sha };
+        } catch {
+            return { check: false };
+        }
+    },
+    update() {
+        let res = this.decrypt(this.enc, this.SHA(this.input.value), this.sha);
+        if (res.check) {
+            this.input.disabled = true;
+            this.input.classList.remove("fail");
+            this.input.classList.add("success");
+            this.content.innerHTML = res.dec;
+            this.content.style.opacity = 1;
+            showimg();
+        } else this.input.classList.add("fail");
+    },
+};
