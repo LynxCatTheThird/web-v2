f56d06d0c1ab634b7d2c28b02b694bf542aa0342
LynxCatTheThird <tsfxlyx@163.com>
2022-12-19 15:35:38 +0800

    换用gitcus为waline

--- person-netlify.bat
+++ person-netlify.bat
@@ -1,6 +1,8 @@
 @echo off
 title 正在复制
-xcopy person person-netlify /e /h /c /i
+ren C:\Users\LynxCatTheThird\web\_config.yml person_netlify_config.yml.
+rd /s /q person-netlify
+xcopy person person-netlify /e /h /c /i 
 title 正在修改配置文件
 rename C:\Users\LynxCatTheThird\web\person_netlify_config.yml _config.yml.
 copy  /Y C:\Users\LynxCatTheThird\web\_config.yml C:\Users\LynxCatTheThird\web\person-netlify\_config.yml
